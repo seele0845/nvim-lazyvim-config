a0eb85b739c86c7f5d3d7fba2143df8aba858015
xxx <1731648650@qq.com>
2026-06-07 19:39:49 +0800

    test del

--- init.lua
+++ init.lua
@@ -6,3 +6,22 @@ vim.lsp.enable("fennel_ls")
 -- vim.lsp.enable("fennel_language_server")
 
 vim.opt.fileformats = "unix,dos"
+
+-- WSL 专用：绑定 win32yank.exe
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "win32yank",
+    copy = {
+      ["+"] = "win32yank.exe -i --crlf",
+      ["*"] = "win32yank.exe -i --crlf",
+    },
+    paste = {
+      ["+"] = "win32yank.exe -o --lf",
+      ["*"] = "win32yank.exe -o --lf",
+    },
+    cache_enabled = false,
+  }
+end
+
+-- 让 y/p 默认走系统剪贴板
+vim.opt.clipboard = "unnamedplus"
